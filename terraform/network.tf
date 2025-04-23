83b036116c8ecefa6653e04d59b2a4add61f4313
NoufAlzubaidi <161399551+NoufAlzubaidi@users.noreply.github.com>
2025-04-23 12:12:20 +0300

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -5,6 +5,19 @@ locals {
   common_tags = {
     environment = local.environment
   }
+
+  web_inbound_ports_map = {
+    "100" = "80",
+    "110" = "443",
+    "120" = "22",
+    "130" = "8501" # Streamlit port
+  }
+
+  vmss_inbound_ports_map = {
+    "100" = "80",
+    "110" = "443",
+    "120" = "22"
+  }
 }
 
 resource "random_string" "myrandom" {
@@ -27,6 +40,7 @@ resource "azurerm_virtual_network" "vnet" {
   tags                = local.common_tags
 }
 
+# Web Subnet Resources
 resource "azurerm_subnet" "websubnet" {
   name                 = "${azurerm_virtual_network.vnet.name}-${var.web_subnet_name}"
   resource_group_name  = azurerm_resource_group.rg.name
@@ -46,16 +60,6 @@ resource "azurerm_subnet_network_security_group_association" "web_subnet_nsg_ass
   network_security_group_id = azurerm_network_security_group.web_subnet_nsg.id
 }
 
-
-locals {
-  web_inbound_ports_map = {
-    "100" = "80",
-    "110" = "443",
-    "120" = "22"
-    "130" = "8501" # Streamlit port
-  }
-}
-
 resource "azurerm_network_security_rule" "web_nsg_rule_inbound" {
   for_each                    = local.web_inbound_ports_map
   name                        = "Rule-Port-${each.value}"
@@ -69,4 +73,47 @@ resource "azurerm_network_security_rule" "web_nsg_rule_inbound" {
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.rg.name
   network_security_group_name = azurerm_network_security_group.web_subnet_nsg.name
+}
+
+# VMSS Subnet Resources
+resource "azurerm_subnet" "vmss_subnet" {
+  name                 = "${azurerm_virtual_network.vnet.name}-${var.vmss_subnet_name}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.vmss_subnet_address
+}
+
+resource "azurerm_network_security_group" "vmss_subnet_nsg" {
+  name                = "${azurerm_subnet.vmss_subnet.name}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "vmss_subnet_nsg_associate" {
+  depends_on                = [azurerm_network_security_rule.vmss_nsg_rule_inbound]
+  subnet_id                 = azurerm_subnet.vmss_subnet.id
+  network_security_group_id = azurerm_network_security_group.vmss_subnet_nsg.id
+}
+
+resource "azurerm_network_security_rule" "vmss_nsg_rule_inbound" {
+  for_each                    = local.vmss_inbound_ports_map
+  name                        = "VMSS-Rule-Port-${each.value}"
+  priority                    = each.key
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = each.value
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.vmss_subnet_nsg.name
+}
+
+# Application Gateway Subnet
+resource "azurerm_subnet" "ag_subnet" {
+  name                 = "${azurerm_virtual_network.vnet.name}-${var.ag_subnet_name}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.ag_subnet_address
 }
