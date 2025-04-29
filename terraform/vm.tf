15a8e2210eadd47a950507646f4cc87990527d2d
NoufAlzubaidi <161399551+NoufAlzubaidi@users.noreply.github.com>
2025-04-29 13:24:01 +0300

    Create vm.tf

---
+++ terraform/vm.tf
@@ -0,0 +1,30 @@
+resource "azurerm_virtual_machine" "tf-vm" {
+  name                         = "tf-vm"
+  location                     = azurerm_resource_group.main.location
+  resource_group_name          = azurerm_resource_group.main.name
+  size                         = "Standard_B2ms" 
+  admin_username               = "azureuser"
+  admin_password               = var.vm_admin_password
+  network_interface_ids        = [azurerm_network_interface.tf-vm_nic.id]
+  storage_os_disk {
+    name              = "tv-os-disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed           = true
+    os_type           = "Linux" 
+  }
+
+  os_profile {
+    computer_name  = "tf-vm"
+    admin_username = "azureuser"
+    admin_password = var.vm_admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
