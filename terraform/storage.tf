30a0bc4e494021fc415a44d3b251b65bb960221e
NoufAlzubaidi <161399551+NoufAlzubaidi@users.noreply.github.com>
2025-04-29 10:50:06 +0300

    Create storage.tf

---
+++ terraform/storage.tf
@@ -0,0 +1,23 @@
+resource "azurerm_storage_account" "main" {
+  name                     = "tfappstorage${random_integer.suffix.result}"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  allow_blob_public_access = false
+
+  tags = {
+    environment = "production"
+  }
+}
+
+resource "random_integer" "suffix" {
+  min = 10000
+  max = 99999
+}
+
+resource "azurerm_storage_container" "files" {
+  name                  = "files"
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "private"
+}
