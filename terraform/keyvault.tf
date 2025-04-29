2bdebf7c3c0f17eba62a59bdf5472fb36f9aa6e1
NoufAlzubaidi <161399551+NoufAlzubaidi@users.noreply.github.com>
2025-04-29 11:01:26 +0300

    Create keyvault.tf

---
+++ terraform/keyvault.tf
@@ -0,0 +1,18 @@
+resource "azurerm_key_vault" "main" {
+  name                        = "tfappkeyvault"
+  location                    = azurerm_resource_group.main.location
+  resource_group_name         = azurerm_resource_group.main.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+  purge_protection_enabled    = true
+  soft_delete_retention_days  = 7
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    secret_permissions = ["Get", "List", "Set", "Delete"]
+  }
+}
+
+data "azurerm_client_config" "current" {}
