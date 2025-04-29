1f0cef9cd2b3cfc5a7354173735aaf55a868b309
NoufAlzubaidi <161399551+NoufAlzubaidi@users.noreply.github.com>
2025-04-29 11:15:08 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,64 @@
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure Region"
+  type        = string
+  default     = "Norway East"
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+  default     = "terraform-sda"
+}
+
 variable "db_password" {
-  description = "Admin password for the PostgreSQL flexible server"
+  description = "The administrator password for PostgreSQL server"
   type        = string
   sensitive   = true
 }
 
-variable "subscription_id" {
-  description = "The Subscription ID for the Azure account"
+variable "vm_admin_username" {
+  description = "Admin username for virtual machines"
   type        = string
+  default     = "azureuser"
+}
+
+variable "vm_admin_password" {
+  description = "Admin password for virtual machines"
+  type        = string
+  sensitive   = true
+}
+
+variable "vmss_instance_count" {
+  description = "Number of VMSS instances"
+  type        = number
+  default     = 2
+}
+
+variable "postgresql_sku" {
+  description = "SKU for the PostgreSQL flexible server"
+  type        = string
+  default     = "B2ms"
+}
+
+variable "postgresql_version" {
+  description = "PostgreSQL version"
+  type        = string
+  default     = "16"
+}
+
+variable "storage_account_tier" {
+  description = "Storage account tier (Standard/Premium)"
+  type        = string
+  default     = "Standard"
+}
+
+variable "storage_replication_type" {
+  description = "Storage replication type (LRS/ZRS/GRS/etc.)"
+  type        = string
+  default     = "LRS"
 }
