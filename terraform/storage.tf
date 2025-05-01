cf5ad769a2e0b88b8adbced22bf7bd788541c290
NoufAlzubaidi <161399551+NoufAlzubaidi@users.noreply.github.com>
2025-05-01 22:14:25 +0300

    Update storage.tf

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,19 +1,19 @@
+resource "random_integer" "suffix" {
+  min = 10000
+  max = 99999
+}
+
 resource "azurerm_storage_account" "main" {
   name                     = "tfappstorage${random_integer.suffix.result}"
   resource_group_name      = azurerm_resource_group.main.name
   location                 = azurerm_resource_group.main.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_tier             = var.storage_account_tier
+  account_replication_type = var.storage_replication_type
   allow_blob_public_access = false
 
   tags = {
     environment = "production"
   }
-}
-
-resource "random_integer" "suffix" {
-  min = 10000
-  max = 99999
 }
 
 resource "azurerm_storage_container" "files" {
