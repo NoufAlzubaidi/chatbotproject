7dbf566037356ada41add41d883f0c1dd4db7c65
NoufAlzubaidi <161399551+NoufAlzubaidi@users.noreply.github.com>
2025-05-01 17:40:10 +0300

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,96 +1,41 @@
+# Virtual Network
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-${var.environment}-${var.app_name}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
 
-
-# تعريف الشبكة الفرعية الخاصة بقاعدة البيانات
-resource "azurerm_subnet" "db_subnet" {
-  name                 = "subnet-db"
+# Subnets
+resource "azurerm_subnet" "vmss" {
+  name                 = "snet-${var.environment}-vmss"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.1.0/24"]
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
 }
 
-# تعريف الشبكة الفرعية الخاصة بـ VMSS
-resource "azurerm_subnet" "vmss_subnet" {
-  name                 = "subnet-vmss"
-  address_prefixes     = ["10.0.2.0/24"]
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-}
-
-# تعريف الشبكة الفرعية الخاصة بـ Application Gateway
-resource "azurerm_subnet" "appgw_subnet" {
-  name                 = "subnet-appgw"
-  address_prefixes     = ["10.0.3.0/24"]
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-}
-
-# تعريف الشبكة الفرعية الخاصة بالجهاز الافتراضي
-resource "azurerm_subnet" "tf-vm_subnet" {
-  name                 = "tf-vm-subnet"
+resource "azurerm_subnet" "appgw" {
+  name                 = "snet-${var.environment}-appgw"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-# تعريف مجموعة الأمان الخاصة بـ VMSS
-resource "azurerm_network_security_group" "vmss_nsg" {
-  name                = "nsg-vmss"
+# Network Security Groups
+resource "azurerm_network_security_group" "vmss" {
+  name                = "nsg-${var.environment}-vmss"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
   security_rule {
-    name                       = "AllowHTTP"
+    name                       = "AllowStreamlit"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "80"
+    destination_port_range     = "8501"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 }
-
-# تعريف مجموعة الأمان الخاصة بالجهاز الافتراضي
-resource "azurerm_network_security_group" "tf-vm_nsg" {
-  name                         = "tf-vm-nsg"
-  location                     = azurerm_resource_group.main.location
-  resource_group_name          = azurerm_resource_group.main.name
-
-  security_rule {
-    name                       = "AllowSSH"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-#bastion 
-
-resource "azurerm_subnet" "bastion_subnet" {
-  name                 = "AzureBastionSubnet"  # الاسم المحدد يجب أن يكون "AzureBastionSubnet"
-  address_prefixes     = ["10.0.4.0/24"]      # اختر النطاق الفرعي المناسب
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-}
-
-resource "azurerm_bastion_host" "terraform" {
-  name                = "terraform-bastion"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  dns_name            = "terraform-bastion"
-
-  # ربط الباستيون بشبكة افتراضية (VNet) والشبكة الفرعية الخاصة به
-  virtual_network_id  = azurerm_virtual_network.main.id
-  subnet_id           = azurerm_subnet.bastion_subnet.id
-
-  tags = {
-    environment = "production"
-  }
-}
-
