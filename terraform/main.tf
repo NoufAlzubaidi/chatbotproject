9c905c8cdf65da3b47e5d994b1fc12469f0bce5b
NoufAlzubaidi <161399551+NoufAlzubaidi@users.noreply.github.com>
2025-05-03 03:25:17 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,16 @@ resource "azurerm_virtual_network" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
+
+resource "azurerm_image" "custom" {
+  name                = "myCustomImage"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  os_disk {
+    os_type  = "Linux"
+    os_state = "Generalized"
+    blob_uri = "chatbotvm_OsDisk_1_5f0d1a1d613a47a684ead74a65687684" 
+    caching  = "ReadWrite"
+  }
+}
