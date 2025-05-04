d6e627ff23bdce89dd4a861c26a54f1ac108fe5e
NoufAlzubaidi <161399551+NoufAlzubaidi@users.noreply.github.com>
2025-05-04 11:50:19 +0300

    Update keyvault.tf

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -1,29 +1,67 @@
-resource "azurerm_key_vault" "kv" {
-name = "local.resource-ame-refix−kv−{random_string.myrandom.id}"
-location = azurerm_resource_group.rg.location
-resource_group_name = azurerm_resource_group.rg.name
-enabled_for_disk_encryption = true
-tenant_id = var.tenant_id
-soft_delete_retention_days = 7
-purge_protection_enabled = false
-sku_name = "standard"
+data "azurerm_client_config" "current" {}
 
-access_policy {
-tenant_id = var.tenant_id
-object_id = var.object_id
+resource "azurerm_key_vault" "kv" {
+  name      = "${local.resource-name-refix}−kv−${random_string.myrandom.id}"
+  location     = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption  = true
+  tenant_id = data.azurerm_client_config.current.tenant_id
+
+  sku_name = "standard"
+}
+
+resource "azurerm_key_vault_access_policy" "myself"{
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azurerm_client_config.current.object_id
+
 
 secret_permissions = [
-  "Get",
-  "List",
-  "Set"
+ "Get",
+ "List",
+ "Set",
+ "Delete",
+ "Backup",
+ "Restore",
+ "Recover",
+ "Purge",
   
-]
-}
-}
-
-resource "azurerm_key_vault_secret" "db_credentials" {
-name = "db-credentials"
-value = "local.db- dministrator-ogin:{local.db_administrator_password}"
-key_vault_id = azurerm_key_vault.kv.id
+  ]
 }
 
+resource "azurerm_key_vault_access_policy" "vmss"{
+  depends_on = [azurerm_linux_virtual_machine_scale_set.web_vmss ]
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = azurerm_linux_virtual_machine_scale_set.web_vmss.identity[0].principal_id
+
+  secret_permissions = [
+   "Get",
+   "List",
+  ]
+
+}
+
+locals {
+  secrets{
+      "PROJ-DB-HOST" = azurerm-postgresql_flexible_server.db.fqdn
+      "PROJ-DB-PORT" = "5432"
+      "PROJ-CHROMADB-HOST" = azurerm_linux_virtual_machine.web_vm.public_ip_address
+      "PROJ-CHROMADB-PORT" = "8000"
+      "PROJ-AZURE-STORAGE-CONTAINER" = azurerm_storage_container.sc.name
+  }
+}
+
+resource "azurerm_key_vault_secret" "s" {
+  depends_on = [azurerm_key_vault_access_policy.myself]
+  for_each = local.secrets 
+  name = each.key  
+  value = each.value
+   key_vault_id = azurerm_key_vault.kv.id
+}
+
+output "keyvault_name" {
+  description = "Key Vault Name"
+  value = azurerm_key_vault.kv.name
+
+}  
