ee54bd2d64c2d69bf7fead6b80571a8e9a498c40
NoufAlzubaidi <161399551+NoufAlzubaidi@users.noreply.github.com>
2025-05-04 13:24:19 +0300

    Update gatewayapp.tf

--- terraform/gatewayapp.tf
+++ terraform/gatewayapp.tf
@@ -1,59 +1,99 @@
-resource "azurerm_application_gateway" "app_gateway" {
-name = "local.resource-ame-refix−appgw−{random_string.myrandom.id}"
-resource_group_name = azurerm_resource_group.rg.name
-location = azurerm_resource_group.rg.location
+resource "azurerm_public_ip" "web_ag_publicip" {
+  name = "${local.resource-name-refix}−web-ag-publicip−${random_string.myrandom.id}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  allocation_method = "Static"
+  sku = "Standard"
+}
 
-sku {
-name = "Standard_v2"
-tier = "Standard_v2"
-capacity = 2
+
+locals{
+  #Generic 
+  frontend_port_name = "${azurerm_virtual_network.vnet.name}-feport"
+  frontend_ip_configuration_name = "${azurerm_virtual_network.vnet.name}-feip"
+  listener_name = "${azurerm_virtual_network.vnet.name}-httplstn"
+  request_routing_rule1_name = "${azurerm_virtual_network.vnet.name}-rqrt-1"
+
+
+  #App1 
+  backend_address_pool_name_app1 = "${azurerm_virtual_network.vnet.name}-beap-app1"
+  http_setting_name_app1 = "${azurerm_virtual_network.vnet.name}-be-htst-app1"
+  probe_name_app1 = "${azurerm_virtual_network.vnet.name}-be-probe-app1"
+}
+
+#resource-2: Azure Application Gateway -standard 
+resource "azurerm_application_gateway" "web_ag" {
+  resource_group_name = azurerm_resource_group.rg.name
+  location =   azurerm_resource_group.rg.location 
+
+#START: -------------------------------#
+#SKU: standard_v2  (new v)
+
+sku { 
+  name = "Standard_v2"
+  tier = "Standard_v2"
+}
+
+autoscale_configuration { 
+  min_capacity = 0
+  min_capacity = 10
 }
 
 gateway_ip_configuration {
-name = "appgw-ip-config"
-subnet_id = azurerm_subnet.websubnet.id
+  name = "my_gateway_ip_configuration"
+  subnet_id = azurerm_subnet.agsubnet.id
 }
 
+# Frontend configs
 frontend_port {
-name = "http-port"
-port = 80
-}
-
-frontend_port {
-name = "https-port"
-port = 443
+  name = local.frontend_port.name
+  port = 80
 }
 
 frontend_ip_configuration {
-name = "appgw-frontend-ip"
-public_ip_address_id = azurerm_public_ip.web_vm_publicip.id
+  name = local.frontend_ip_configuration_name
+  public_ip_address_id = azurerm_public_ip.wbe_ag_publicip.id
 }
 
-backend_address_pool {
-name = "vmss-backend-pool"
+#Listener : HTTP 80 
+http_listener {
+  name = local.listener_name 
+  frontend_ip_configuration_name = local.frontend_ip_configuration_name
+  frontend_port_name = local.frontend_port_name
+  protocol = "Http"
+}
+
+#App1 configs
+backend_address_pool { 
+  name = local.backend_address_pool_name_app1
 }
 
 backend_http_settings {
-name = "http-settings"
-cookie_based_affinity = "Disabled"
-port = 80
-protocol = "Http"
-request_timeout = 60
+  name = local.http_setting_name_app1
+  cookie_based_affinity = "Disabled"
+  port = 8501
+  protocol = "Http"
+  request_timeout = 60 
+  probe_name = local.probe_name_app1 
+}
+probe {
+  name =local.probe_name_app1
+  host = "127.0.0.1"
+  interval = 30 
+  timeout = 30
+  unhealthy_threshold = 3
+  protocol = "Http"
+  port = 8501
+  path = "/"
 }
 
-http_listener {
-name = "http-listener"
-frontend_ip_configuration_name = "appgw-frontend-ip"
-frontend_port_name = "http-port"
-protocol = "Http"
+# Rule-1 
+request_routing_rule { 
+  name = local.request_routing_rule1_name
+  priority = 9 
+  rule_type = "Basic"
+  http_listener_name = local.backend_address_pool_name_app1
+  backend_http_settings_name = local.http_setting_name_app1
+}
 }
 
-request_routing_rule {
-name = "http-rule"
-rule_type = "Basic"
-http_listener_name = "http-listener"
-backend_address_pool_name = "vmss-backend-pool"
-backend_http_settings_name = "http-settings"
-priority = 100
-}
-}
