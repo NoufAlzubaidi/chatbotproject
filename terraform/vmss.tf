80436778fc906e860802bf522a0754a1fb135ab6
NoufAlzubaidi <161399551+NoufAlzubaidi@users.noreply.github.com>
2025-05-04 17:14:13 +0300

    Update vmss.tf

--- terraform/vmss.tf
+++ terraform/vmss.tf
@@ -1,8 +1,13 @@
+loacls { 
+source_image_id = source_image_id = "/subscriptions/9896598a-01c4-4709-b370-042f72f0e1ce/resourceGroups/my-rg/providers/Microsoft.Compute/images/myCustomImage"
+}
+
+
 resource "azurerm_linux_virtual_machine_scale_set" "web_vmss" {
-  name                = "${local.resource_name_prefix}-vmss-${random_string.myrandom.id}"
+  name                = "${local.resource_name_prefix}-web-vmss-${random_string.myrandom.id}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  sku                 = "Standard_B2ms"
+  sku                 = "Standard_D2s_v6"
   instances           = 2
   admin_username      = "azureuser"
 
@@ -11,32 +16,106 @@ resource "azurerm_linux_virtual_machine_scale_set" "web_vmss" {
     public_key = file("${path.module}/ssh-keys/terraform-azure.pub")
   }
 
-  source_image_reference {
-    publisher = "canonical"
-    offer     = "ubuntu-24_04-lts"
-    sku       = "server"
-    version   = "latest"
-  }
+ # source_image_reference {
+  #  publisher = "canonical"
+   # offer     = "ubuntu-24_04-lts"
+   # sku       = "server"
+    #version   = "latest"
+ # }
+
+source_image_id = local.source_image_id
 
   os_disk {
     storage_account_type = "Premium_LRS"
     caching              = "ReadWrite"
   }
 
+  upgrade_mode = "Automatic"
+  secure_boot_enabled = true 
+  
+  identity {
+   type = "SystemAssigned"
+  }
+
   network_interface {
-    name    = "vmss-nic"
+    name    = "web-vmss-nic"
     primary = true
 
     ip_configuration {
       name      = "internal"
       primary   = true
-      subnet_id = azurerm_subnet.websubnet.id
-    }
+      ip_configuration {
+        name = "internal"
+        primary = true
+        subnet_id = azurerm_subnet.websubnet.id
+        application_gateway_backend_address_pool_ids = [tolist(azurerm_application_gateway.web_ag.backend_address_pool)[0].id]
+          }
+      }  
   }
 
-  custom_data = base64encode(file("${path.module}/init_script.sh"))
+output "web_vmss_id" {
+  description = "Web Virtual Machine Scale Set ID "
+  value = azurerm_linux_virtual_machine_scale_set.web_vmss.id
+}
 
-  lifecycle {
-    ignore_changes = [instances]
+resource "azurerm_monitor_autoscale_setting" "autoscale" {
+  name = "myautoscaleSetting"
+  resource_group_name = azurerm_resource_group.rg.name
+   location            = azurerm_resource_group.rg.location
+  target_resource_id = azurerm_linux_virtual_machine_scale_set.web_vmss.id
+
+}
+
+profile {
+  name ="defaultprofile"
+  capacity {
+    default = 2
+    minimum = 2 
+    maximum = 3
+  }
+
+#scale-out 1 instance if the cpu is greater than 75%
+rule {
+  metric_trigger {
+    metric_name = "percentage CPU"
+     metric_resource_id = azurerm_linux_virtual_machine_scale_set.web_vmss.id
+    time_grain = "PT1M" # 1min 
+    statistic = "Average"
+    time_window = "PT5M" # 5min
+    time_aggregation = "Average"
+    operator = 80 
+    metric_namespace = "microsoft.compute/virtualmachinescalesets"
+
+    }
+
+scale_action {
+  direction = "Increase"
+  type = "ChangeCount"
+  value = "1"
+  cooldown = "PT1M"
   }
 }
+
+#scale-out 1 instance if the cpu is less than 25%
+rule {
+  metric_trigger {
+    metric_name = "percentage CPU"
+    metric_resource_id = azurerm_linux_virtual_machine_scale_set.web_vmss.id
+    time_grain = "PT1M" # 1min 
+    statistic = "Average"
+    time_window = "PT5M" # 5min
+    time_aggregation = "Average"
+    operator = LessThan
+    threshold = 20 
+   }
+    scale_action {
+      direction = "Decrease"
+      type = "ChangeCount"
+      value = "1"
+      cooldown = "PT1M"
+      }
+    }
+  }  
+}
+ 
+
