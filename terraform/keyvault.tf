6ea64099277f96bc93d42cc8690b1ed21d682ac4
NoufAlzubaidi <161399551+NoufAlzubaidi@users.noreply.github.com>
2025-05-05 18:09:20 +0300

    Update keyvault.tf

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -1,7 +1,7 @@
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "kv" {
-  name      = "${local.resource-name-refix}−kv−${random_string.myrandom.id}"
+  name      = "${local.resource-name-prefix}−kv−${random_string.myrandom.id}"
   location     = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   enabled_for_disk_encryption  = true
