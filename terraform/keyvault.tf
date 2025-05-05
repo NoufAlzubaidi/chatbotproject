eb61eb3ce58095a9caffb2a506c36dd8426a99e3
NoufAlzubaidi <161399551+NoufAlzubaidi@users.noreply.github.com>
2025-05-05 19:49:49 +0300

    Update keyvault.tf

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -44,7 +44,7 @@ resource "azurerm_key_vault_access_policy" "vmss"{
 
 locals {
   secrets = {
-      "PROJ-DB-HOST" = azurerm-postgresql_flexible_server.db.fqdn
+      "PROJ-DB-HOST" = azurerm_postgresql_flexible_server.db.fqdn
       "PROJ-DB-PORT" = "5432"
       "PROJ-CHROMADB-HOST" = azurerm_linux_virtual_machine.web_vm.public_ip_address
       "PROJ-CHROMADB-PORT" = "8000"
