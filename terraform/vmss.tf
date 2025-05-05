9caa1a47c84b0c3d3307ba70dddbc8a772d1a24e
NoufAlzubaidi <161399551+NoufAlzubaidi@users.noreply.github.com>
2025-05-05 22:38:24 +0300

    Update vmss.tf

--- terraform/vmss.tf
+++ terraform/vmss.tf
@@ -7,7 +7,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "web_vmss" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Standard_B1s"
-  instances           = 2
+  instances           = 1
   admin_username      = "azureuser"
 
   admin_ssh_key {
