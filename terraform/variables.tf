4290b9022119f0e4ca03c553ae0155bca0c225f8
NoufAlzubaidi <161399551+NoufAlzubaidi@users.noreply.github.com>
2025-05-05 18:30:45 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -39,6 +39,19 @@ variable "web_subnet_address" {
   default     = ["10.0.1.0/24"]
 }
 
+variable "ag_subnet_name" {
+  description = "Name of the Application Gateway subnet"
+  type        = string
+  default     = "agsubnet"
+}
+
+variable "ag_subnet_address" {
+  description = "Address space for Application Gateway subnet"
+  type        = list(string)
+  default     = ["10.0.2.0/24"]
+}
+
+
 variable "environment" {
   description = "Environment name (dev, staging, prod)"
   type        = string
