323f12fafce7290a5dfdf5da46df9f290afafaa5
NoufAlzubaidi <161399551+NoufAlzubaidi@users.noreply.github.com>
2025-05-05 21:28:27 +0300

    Update vm.tf

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -1,19 +1,19 @@
 resource "azurerm_public_ip" "web_vm_publicip" {
- name = "${var.resource_prefix}-${var.environment}-web-vm-${random_string.myrandom.id}"
+  name                = "${var.resource_prefix}-${var.environment}-web-vm-${random_string.myrandom.id}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   allocation_method   = "Static"
   sku                 = "Standard"
-  domain_name_label   = "app1-vm-${random_string.myrandom.id}"
+  domain_name_label   = "${var.resource_prefix}-${var.environment}-vm-${random_string.myrandom.id}"  # تعديل هنا لضمان التفرد
 }
 
 resource "azurerm_network_interface" "web_vm_nic" {
-  name                = "${local.resource_name_prefix}-web-vm-nic-${random_string.myrandom.id}"
+  name                = "${var.resource_prefix}-${var.environment}-web-vm-nic-${random_string.myrandom.id}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "web-vm-ip-1"
+    name                          = "internal"
     subnet_id                     = azurerm_subnet.websubnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.web_vm_publicip.id
@@ -21,26 +21,34 @@ resource "azurerm_network_interface" "web_vm_nic" {
 }
 
 resource "azurerm_linux_virtual_machine" "web_vm" {
-  name = "${local.resource_name_prefix}-web-vm-${random_string.myrandom.id}"
-  #computer_name = "web-vm" # Hostname of the VM (Optional)
-  resource_group_name   = azurerm_resource_group.rg.name
-  location              = azurerm_resource_group.rg.location
-  size                  = "Standard_B2ms"
-  admin_username        = "azureuser"
+  name                = "${var.resource_prefix}-${var.environment}-web-vm-${random_string.myrandom.id}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2ms"
+  admin_username      = "azureuser"  
+
   network_interface_ids = [azurerm_network_interface.web_vm_nic.id]
-  custom_data = base64encode(file("${path.module}/init_script.sh"))
+  
+  custom_data = filebase64("${path.module}/init_script.sh")
+  
   admin_ssh_key {
     username   = "azureuser"
     public_key = file("${path.module}/ssh-keys/terraform-azure.pub")
   }
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Premium_LRS"
   }
+
   source_image_reference {
-    publisher = "canonical"
-    offer     = "ubuntu-24_04-lts"
-    sku       = "server"
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
     version   = "latest"
+  }
+
+  identity {
+    type = "SystemAssigned"
   }
 }
