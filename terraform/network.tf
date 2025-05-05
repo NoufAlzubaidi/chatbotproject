2baf51014593f8114ce67c8f1da891039b5e28ee
NoufAlzubaidi <161399551+NoufAlzubaidi@users.noreply.github.com>
2025-05-05 19:33:17 +0300

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -5,6 +5,17 @@ locals {
   common_tags = {
     environment = local.environment
   }
+  
+  web_inbound_ports_map = {
+    "120" = "22"
+    "130" = "8501" # Streamlit port
+  }
+  
+  ag_inbound_ports_map = {
+    "100" = "80"
+    "110" = "443"
+    "130" = "65200-65535"
+  }
 }
 
 resource "random_string" "myrandom" {
@@ -13,12 +24,14 @@ resource "random_string" "myrandom" {
   special = false
 }
 
+# Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "${local.resource_name_prefix}-${var.resource_group_name}-${random_string.myrandom.id}"
   location = var.resource_group_location
   tags     = local.common_tags
 }
 
+# Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "${local.resource_name_prefix}-${var.vnet_name}-${random_string.myrandom.id}"
   address_space       = var.vnet_address_space
@@ -27,6 +40,7 @@ resource "azurerm_virtual_network" "vnet" {
   tags                = local.common_tags
 }
 
+# Web Subnet
 resource "azurerm_subnet" "websubnet" {
   name                 = "${azurerm_virtual_network.vnet.name}-${var.web_subnet_name}"
   resource_group_name  = azurerm_resource_group.rg.name
@@ -34,24 +48,11 @@ resource "azurerm_subnet" "websubnet" {
   address_prefixes     = var.web_subnet_address
 }
 
+# Web Subnet NSG
 resource "azurerm_network_security_group" "web_subnet_nsg" {
   name                = "${azurerm_subnet.websubnet.name}-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet_network_security_group_association" "web_subnet_nsg_associate" {
-  depends_on                = [azurerm_network_security_rule.web_nsg_rule_inbound]
-  subnet_id                 = azurerm_subnet.websubnet.id
-  network_security_group_id = azurerm_network_security_group.web_subnet_nsg.id
-}
-
-
-locals {
-  web_inbound_ports_map = {
-    "120" = "22"
-    "130" = "8501" # Streamlit port
-  }
 }
 
 resource "azurerm_network_security_rule" "web_nsg_rule_inbound" {
@@ -69,38 +70,26 @@ resource "azurerm_network_security_rule" "web_nsg_rule_inbound" {
   network_security_group_name = azurerm_network_security_group.web_subnet_nsg.name
 }
 
-#Application Gateway subnet 
+resource "azurerm_subnet_network_security_group_association" "web_subnet_nsg_associate" {
+  depends_on                = [azurerm_network_security_rule.web_nsg_rule_inbound]
+  subnet_id                 = azurerm_subnet.websubnet.id
+  network_security_group_id = azurerm_network_security_group.web_subnet_nsg.id
+}
+
+# Application Gateway Subnet 
 resource "azurerm_subnet" "agsubnet" {
-name       = "${azurerm_virtual_network.vnet.name}-${var.ag_subnet_name}-${random_string.myrandom.id}"
-resource_group_name = azurerm_resource_group.rg.name
-resource_network_name = azurerm_virtual_network.vnet.name
-address_prefixes = var.ag_subnet_address
+  name                 = "${azurerm_virtual_network.vnet.name}-${var.ag_subnet_name}-${random_string.myrandom.id}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.ag_subnet_address
 }
 
-#Network security group 
+# AG Subnet NSG
 resource "azurerm_network_security_group" "ag_subnet_nsg" {
-name   = "${azurerm_subnet.agsubnet.name}-nsg"
-location = azurerm_resource_group.rg.location 
-resource_group_name = azurerm_resource_group.rg.name
+  name                = "${azurerm_subnet.agsubnet.name}-nsg"
+  location            = azurerm_resource_group.rg.location 
+  resource_group_name = azurerm_resource_group.rg.name
 }
-
-#Associate NSG and Subnet
-resource "azurerm_subnet_network_security_group_associaation" "ag_subnet_nsg_associate" {
-depends_on  = [azurerm_network_security_rule.ag_nsg_rule_inbound] #every nsg association will disassaciate nsg from subnet
-subnet_id  = azure_subnet.agsubnet.id
-network_security_group_id =  azurerm_network_security_group.ag_subnet_nsg.id 
-}
-
-#create nsg rules 
-#locals block for security rules 
-locals{
-  ag_inbound_ports_map = {
-  "100" : "80"
-  "110" : "443"
-  "130" : "65200-65535"
-  }
-}
-
 
 resource "azurerm_network_security_rule" "ag_nsg_rule_inbound" {
   for_each                    = local.ag_inbound_ports_map
@@ -115,4 +104,11 @@ resource "azurerm_network_security_rule" "ag_nsg_rule_inbound" {
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.rg.name
   network_security_group_name = azurerm_network_security_group.ag_subnet_nsg.name
+}
+
+# Associate AG NSG and Subnet
+resource "azurerm_subnet_network_security_group_association" "ag_subnet_nsg_associate" {
+  depends_on                = [azurerm_network_security_rule.ag_nsg_rule_inbound]
+  subnet_id                 = azurerm_subnet.agsubnet.id
+  network_security_group_id = azurerm_network_security_group.ag_subnet_nsg.id
 }
