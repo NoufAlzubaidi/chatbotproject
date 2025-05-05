8c5f52cdb283f5c3313703056ebfea839cd4aa1e
NoufAlzubaidi <161399551+NoufAlzubaidi@users.noreply.github.com>
2025-05-05 18:58:42 +0300

    Update gatewayapp.tf

--- terraform/gatewayapp.tf
+++ terraform/gatewayapp.tf
@@ -1,5 +1,5 @@
 resource "azurerm_public_ip" "web_ag_publicip" {
- name = "chatbot-${var.environment}-web-ag-publicip-${random_string.myrandom.id}"
+ name = "${var.resource_prefix}-${var.environment}-web-ag-publicip-${random_string.myrandom.id}"
   resource_group_name = azurerm_resource_group.rg.name
   location = azurerm_resource_group.rg.location
   allocation_method = "Static"
